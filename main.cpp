9293f70ba7ef06ae5d15dbb7614a673fa5a2020a
JernejL123 <jernej.lobnik@gmail.com>
2024-10-20 13:01:15 +0200

    Dodana funkcija v novi veji

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 # include <iostream>
 
+int sestej(int a,int b){
+    return a + b;
+}
+
 int main(){
     std::cout << "To je super Mini projekt" << std::endl;
+    std::cout << sestej(5,6);
     return 0;
 }
